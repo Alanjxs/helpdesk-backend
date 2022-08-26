07738d612f89ebca49a2fc61c6eb8e607159d287
alanjxs@gmail.com <alanjxs@gmail.com>
2022-08-26 13:23:29 -0300

    atualizando os dados iniciais

--- src/main/java/com/valdir/helpdesk/services/DBService.java
+++ src/main/java/com/valdir/helpdesk/services/DBService.java
@@ -27,30 +27,28 @@ public class DBService {
 	
 	
 	public void instanciaDB() {
-		Tecnico tec1 = new Tecnico(null, "Valdir Cersar", "871.839.570-80", "valdir@gmail.com", encoder.encode("123"));
+		Tecnico tec1 = new Tecnico(null, "Valdir Cezar", "550.482.150-95", "valdir@mail.com", encoder.encode("123"));
 		tec1.addPerfil(Perfil.ADMIN);
-		Tecnico tec2 = new Tecnico(null, "Alanjxs", "025.984.083-14", "alanjxs@gmail.com", encoder.encode("123"));
-		Tecnico tec3 = new Tecnico(null, "Vandame", "568.891.760-14", "vandame@gmail.com", encoder.encode("123"));
-		Tecnico tec4 = new Tecnico(null, "Bruce Lee", "478.285.300-99", "bruce@gmail.com", encoder.encode("123"));
-		Tecnico tec5 = new Tecnico(null, "Jack Chan", "753.790.970-90", "jack@gmail.com", encoder.encode("123"));
-		
-		
-		Cliente cli1 = new Cliente(null, "Linus Torvalds", "477.144.690-30", "linus@gmail.com", encoder.encode("123"));
-		Cliente cli2 = new Cliente(null, "Bill Gates", "225.634.540-85", "bill@gmail.com", encoder.encode("123"));
-		Cliente cli3 = new Cliente(null, "Steve Jobs", "538.239.180-79", "steve@gmail.com", encoder.encode("123"));
-		Cliente cli4 = new Cliente(null, "Elon Musk", "510.513.740-06", "elon@gmail.com", encoder.encode("123"));
-		Cliente cli5 = new Cliente(null, "mark Zuckberg", "795.407.150-24", "mark@gmail.com", encoder.encode("123"));
+		Tecnico tec2 = new Tecnico(null, "Richard Stallman", "903.347.070-56", "stallman@mail.com", encoder.encode("123"));
+		Tecnico tec3 = new Tecnico(null, "Claude Elwood Shannon", "271.068.470-54", "shannon@mail.com", encoder.encode("123"));
+		Tecnico tec4 = new Tecnico(null, "Tim Berners-Lee", "162.720.120-39", "lee@mail.com", encoder.encode("123"));
+		Tecnico tec5 = new Tecnico(null, "Linus Torvalds", "778.556.170-27", "linus@mail.com", encoder.encode("123"));
 
-		
-		Chamado c1 = new Chamado(null, Prioridade.MEDIA , Status.ANDAMENTO, "Chamado 01", "Primeiro Chamado", tec1, cli1);
-		Chamado c2 = new Chamado(null, Prioridade.BAIXA , Status.ABERTO, "Chamado 02", "Segundo Chamado", tec2, cli2);
-		Chamado c3 = new Chamado(null, Prioridade.ALTA , Status.ABERTO, "Chamado 03", "terceiro Chamado", tec3, cli3);
-		Chamado c4 = new Chamado(null, Prioridade.BAIXA , Status.ANDAMENTO, "Chamado 04", "quarto Chamado", tec4, cli4);
-		Chamado c5 = new Chamado(null, Prioridade.MEDIA , Status.ANDAMENTO, "Chamado 05", "quinto Chamado", tec4, cli5);
-		
-		
+		Cliente cli1 = new Cliente(null, "Albert Einstein", "111.661.890-74", "einstein@mail.com", encoder.encode("123"));
+		Cliente cli2 = new Cliente(null, "Marie Curie", "322.429.140-06", "curie@mail.com", encoder.encode("123"));
+		Cliente cli3 = new Cliente(null, "Charles Darwin", "792.043.830-62", "darwin@mail.com", encoder.encode("123"));
+		Cliente cli4 = new Cliente(null, "Stephen Hawking", "177.409.680-30", "hawking@mail.com", encoder.encode("123"));
+		Cliente cli5 = new Cliente(null, "Max Planck", "081.399.300-83", "planck@mail.com", encoder.encode("123"));
+ 
+		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 1", "Teste chamado 1", tec1, cli1);
+		Chamado c2 = new Chamado(null, Prioridade.ALTA, Status.ABERTO, "Chamado 2", "Teste chamado 2", tec1, cli2);
+		Chamado c3 = new Chamado(null, Prioridade.BAIXA, Status.ENCERRADO, "Chamado 3", "Teste chamado 3", tec2, cli3);
+		Chamado c4 = new Chamado(null, Prioridade.ALTA, Status.ABERTO, "Chamado 4", "Teste chamado 4", tec3, cli3);
+		Chamado c5 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 5", "Teste chamado 5", tec2, cli1);
+		Chamado c6 = new Chamado(null, Prioridade.BAIXA, Status.ENCERRADO, "Chamado 7", "Teste chamado 6", tec1, cli5);
+
 		pessoaRepository.saveAll(Arrays.asList(tec1, tec2, tec3, tec4, tec5, cli1, cli2, cli3, cli4, cli5));
-		chamadoRepository.saveAll(Arrays.asList(c1, c2, c3, c4, c5));
+		chamadoRepository.saveAll(Arrays.asList(c1, c2, c3, c4, c5, c6));
 		
 	}
 
